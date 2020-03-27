fcd9e991ba7bde334bdb2d86900cda7d75728daf
Vladimir Khramov <khramov.vladimir@gmail.com>
2020-03-27 10:18:25 +0300

    Lesson 7_2 solve Added file exists check

--- tasks07/task_7_2.py
+++ tasks07/task_7_2.py
@@ -4,9 +4,12 @@ wget --no-check-certificate https://raw.githubusercontent.com/natenka/pyneng-exa
 author Vladimir Khramov
 
 """
+import os
+
 filename = input("Введите имя файла ")
 
 ignore = ['duplex', 'alias', 'Current configuration']
+
 
 def config_commands(filename):
     with open(filename, 'r') as f:
@@ -18,7 +21,22 @@ def config_commands(filename):
                 print(line)
 
 
-if filename:
-    config_commands(filename)
-else:
-    config_commands('config_sw1.txt')
+def config_print():
+    if filename:
+        if check_if_file_exists(filename):
+            config_commands(filename)
+        else:
+            print("No such file")
+    else:
+        config_commands('config_sw1.txt')
+
+
+def check_if_file_exists(filename):
+    for x in os.listdir('.'):
+        if filename in x:
+            return True
+        else:
+            return False
+
+
+config_print()
