d5a8a088641b17723eaf7bc89c88513b0fb3a1a0
Vladimir Khramov <khramov.vladimir@gmail.com>
2020-03-27 09:20:36 +0300

    Changed some files

---
+++ tasks07/task_7_1.py
@@ -0,0 +1,12 @@
+
+
+# Задание 4.6
+# Обработать строку ospf_route и вывести информацию на стандартный поток вывода в виде:
+#
+# Protocol:               OSPF
+# Prefix:                 10.0.24.0/24
+# AD/Metric:              110/41
+# Next-Hop:               10.0.13.3
+# Last update:            3d18h
+# Outbound Interface:     FastEthernet0/0
+
