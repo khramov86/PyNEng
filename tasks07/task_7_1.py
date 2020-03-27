e65fb1da71f243ecf8b005688dd652e09b721daa
Vladimir Khramov <khramov.vladimir@gmail.com>
2020-03-27 09:50:04 +0300

    Lesson 7_1 solve

--- tasks07/task_7_1.py
+++ tasks07/task_7_1.py
@@ -1,5 +1,3 @@
-
-
 # Задание 4.6
 # Обработать строку ospf_route и вывести информацию на стандартный поток вывода в виде:
 #
@@ -10,3 +8,17 @@
 # Last update:            3d18h
 # Outbound Interface:     FastEthernet0/0
 
+with open('ospf.txt', 'r') as f:
+    for line in f:
+        # strip дает отображение по строкам
+        # split раделяет строку по пробелу и откидывает все лишнии пробелы
+        line = line.split()
+        if line[0] == 'O':
+            print("Protocol:\t {}".format("OSPF"))
+            print("Prefix:\t {}".format(line[1]))
+            print("AD/Metric: :\t {}".format(''.join(line[2:3]).strip('[').strip(']')))
+            print("Next-Hop:\t {}".format(line[4].strip(',')))
+            print("Last update:\t {}".format(line[5].strip(',')))
+            print("Outbound Interface:\t {}".format(line[6].strip(',')))
+        else:
+            print("Protocol not found")
