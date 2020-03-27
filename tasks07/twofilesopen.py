2c0911fedf2c146045f3e40578d92d6bb0f24f69
khramov86 <khramov.vladimir@gmail.com>
2020-03-27 07:29:48 +0300

    Operations with files lessons

--- tasks07/twofilesopen.py
+++ tasks07/twofilesopen.py
@@ -5,3 +5,10 @@ with open('r1.txt', 'r') as src, open('result.txt', 'r+') as dest:
             dest.write(line)
     dest.seek(0)
     print(dest.read())
+
+# Same as
+# with open('r1.txt') as src:
+#     with open('result.txt', 'w') as dest:
+#         for line in src:
+#             if line.startswith('service'):
+#                 dest.write(line)
