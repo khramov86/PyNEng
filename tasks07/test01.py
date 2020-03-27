68e674dde861c3c8b4ab534f58472709d58631a5
khramov86 <khramov.vladimir@gmail.com>
2020-03-27 06:58:23 +0300

    Python fileread examples test

--- tasks07/test01.py
+++ tasks07/test01.py
@@ -2,6 +2,16 @@ f = open('r1.txt', 'r')
 
 test = f.read()
 print(test)
-print("Print without seeking")
-#Ничего не выведется, т.к. файл не промотан назад
+print("Что будет если вывести файл без перемотки")
+# Ничего не выведется, т.к. файл не промотан назад
 print(f.read())
+f.seek(0)
+print("Тестирование чтение файла после перемотки")
+print(f.read())
+
+print("Снова промотаем файл")
+f.seek(0)
+print("прочитаем строчку")
+print(f.readline())
+print("прочитаем еще одну строчку")
+print(f.readline())
