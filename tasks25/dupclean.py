109236a9766849c748517fe39ec48227466dae7c
Vladimir Khramov <khramov.vladimir@gmail.com>
2020-04-01 14:35:57 +0300

    Clean dups test and calling it from class

--- tasks25/dupclean.py
+++ tasks25/dupclean.py
@@ -1,28 +1,17 @@
-topology_example = {
-    ('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
-    ('R2', 'Eth0/0'): ('SW1', 'Eth0/2'),
-    ('R2', 'Eth0/1'): ('SW2', 'Eth0/11'),
-    ('R1', 'Eth0/0'): ('SW1', 'Eth0/1'),
-    ('R3', 'Eth0/0'): ('SW1', 'Eth0/3'),
-    ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
-    ('R3', 'Eth0/1'): ('R4', 'Eth0/0'),
-    ('R3', 'Eth0/2'): ('R5', 'Eth0/0'),
-    ('SW1', 'Eth0/1'): ('R1', 'Eth0/0'),
-    ('SW1', 'Eth0/2'): ('R2', 'Eth0/0'),
-    ('SW1', 'Eth0/3'): ('R3', 'Eth0/0'),
-    ('SW1', 'Eth0/2'): ('R2', 'Eth0/0')
-}
+# Obtain dictionary and clean it if
+# key == key and key == value
 
-cleaned_dict = {}
-start = True
-for key, value in topology_example.items():
-    if start:
-        cleaned_dict[key] = value
-        start = False
-    else:
-        if key in cleaned_dict.keys() or value in cleaned_dict.keys():
-            continue
-        else:
+
+def clean_dups(topology_example):
+    cleaned_dict = {}
+    start = True
+    for key, value in topology_example.items():
+        if start:
             cleaned_dict[key] = value
-for key, value in cleaned_dict.items():
-    print(key, value)
+            start = False
+        else:
+            if key in cleaned_dict.keys() or value in cleaned_dict.keys():
+                continue
+            else:
+                cleaned_dict[key] = value
+    return cleaned_dict
--- tasks25/hw_25_1.py
+++ tasks25/hw_25_1.py
@@ -1,42 +1,11 @@
+import tasks25.dupclean as dups
 class Topology:
     def __init__(self, topology):
         self.topology = topology
 
     def remove_duplicates(self):
-       # print(self.topology)
-        temp_dict = {}
-        for key, value in self.topology.items():
-           # print(key, value)
-           # добавить один элемент
-            if temp_dict:
-                temp_dict[key] = value
-             #   print(temp_dict)
-            else:
-                if key in temp_dict.keys():
-
-                    print("There is such key or value")
-                elif key in temp_dict.values():
-                        print("There is such key or value")
-                else:
-                    temp_dict[key] = value
-               # print(temp_dict)
-            # else:
-            #     if temp_dict[key] != self.topology_example[key]:
-            #         continue
-            #     else:
-            #         temp_dict[key] = value
-            # else:
-            #     if topology_example[key] == temp_dict[key]:
-            #         continue
-            #     elif topology_example[value] == temp_dict[key]:
-            #         continue
-            #     else:
-            #         temp_dict[key] = value
-        for key,value in temp_dict.items():
-            print(key, value)
-
-
-
+        print(self.topology)
+        print(dups.clean_dups(self.topology))
 
 """Дублем считается ситуация, когда в словаре есть такие пары: ('R1', 'Eth0/0'): ('SW1', 'Eth0/1')
  и ('SW1','Eth0/1'): ('R1', 'Eth0/0') """
