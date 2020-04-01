dd62906995c516cdab80988e13bafbd64fc7d567
Vladimir Khramov <khramov.vladimir@gmail.com>
2020-04-01 11:35:51 +0300

    Tables exists check

--- tasks25/demo01.py
+++ tasks25/demo01.py
@@ -3,7 +3,7 @@ import sqlite3
 db = sqlite3.connect('.\\dhcp_snooping.db')
 # Переменная conn - это объект, который представляет реальное соединение с БД. Благодаря функции type, можно выяснить
 # экземпляром какого класса является объект conn:
-print(type(db))
+# print(type(db))
 # db.in_transaction проверить, был ли коммит (возвращает True или False)
 def init_table():
     db.execute("""
@@ -16,12 +16,26 @@ def init_table():
 
     db.execute("""
     alter table dhcp add vlan TEXT
-    """)
+                """)
 
 
 query = 'insert into dhcp (mac, ip, vlan, interface) values (?, ?, ?, ?)'
-db.execute(query, ('0000.1111.7777', '10.255.1.1', '10', 'Gi0/7'))
-db.execute("commit")
+
+
+# Выборка из базы
+result = db.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='dhcp'").fetchone()
+# Если есть поле - тип будет tuple
+# print(type(result))
+if result is None:
+    print("Инициализирую таблицу")
+    init_table()
+else:
+    db.execute(query, ('0000.1111.7777', '10.255.1.1', '10', 'Gi0/7'))
+    # Здесь метод вернет True
+    print(db.in_transaction)
+    db.execute("commit")
+    # Здесь метод вернет False
+    print(db.in_transaction)
 db.close()
 
 # print(db.in_transaction)
